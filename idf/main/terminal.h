e2d10fabf0d5f0b1eca9ab32469ec45436579572
aqbifzl <deserts_farrier_1c@icloud.com>
2025-12-08 20:00:32 +0100

    move ino code to root and remove idf

--- idf/main/terminal.h
+++
@@ -1,8 +0,0 @@
-#ifndef TERMINAL_H
-#define TERMINAL_H
-
-void terminal_init(void);
-void terminal_redraw_info(void);
-void terminal_redraw_spectrum(void);
-
-#endif // !TERMINAL_H
--- idf/main/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-#define COUNT_OF(x)                                                            \
-  ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
-
-#endif
